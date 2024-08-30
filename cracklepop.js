2309831b277a11314f10af7e0a58705dd515280e
Neil Kandalgaonkar <neilk@neilk.net>
2024-08-30 07:25:25 -0700

    explicate

--- cracklepop.js
+++ cracklepop.js
@@ -1,10 +1,18 @@
 [...Array(101).keys()]
     .splice(1)
-    .map(i => [i, i])
-    .map(p =>
+    .map(index => [index, index])
+    .map(pair =>
         [[5, 'Pop'], [3, 'Crackle']]
             .map(([modulus, label]) =>
-                ([i, display]) => [i, i % modulus ? display : isNaN(display) ? label + display : label])
-            .reduce((y, f) => f(y), p)
+                ([index, display]) => [
+                    index,
+                    index % modulus
+                        ? display
+                        : isNaN(display)
+                            ? label + display
+                            : label
+                ]
+            )
+            .reduce((item, fn) => fn(item), pair)
     )
-    .forEach(p => console.log(p[1]));
+    .forEach(pair => console.log(pair[1]));
